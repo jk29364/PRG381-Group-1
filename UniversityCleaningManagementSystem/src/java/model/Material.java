d83bd11a8cf4fba44dc7139662767730f48ec2ca
Masilo Pudikabekwa <603037@student.belgiumcampus.ac.za>
2026-07-22 02:01:08 +0200

    Add Material model class
    Add Material POJO class for the inventory management module. The class represents a material record with fields for ID, name, category, quantity, reorder level, and supplier ID. Includes validation in setters to prevent negative stock and reorder values, and a helper method to check if stock is low.

---
+++ UniversityCleaningManagementSystem/src/java/model/Material.java
@@ -0,0 +1,98 @@
+package model;
+
+/**
+ * Material.java
+ * Model (POJO) class representing a Material record.
+ * Part of: Inventory management Module (Team Member 2)
+ */
+public class Material {
+    
+    //Variables
+    private int materialTd;
+    private String materialName;
+    private String category;
+    private int quantity;
+    private int reorderLevel;
+    private int supplierId;
+    
+    //Default constructor
+    public Material(){}
+    
+    //Constructor with parameters
+    public Material(int materialTd, String materialName, String category, int quantity, int reorderLevel, int supplierId) {
+        this.materialTd = materialTd;
+        this.materialName = materialName;
+        this.category = category;
+        this.quantity = quantity;
+        this.reorderLevel = reorderLevel;
+        this.supplierId = supplierId;
+    }
+    
+    //Getters  
+    public int getMaterialTd() {
+        return materialTd;
+    }
+
+    public String getMaterialName() {
+        return materialName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getReorderLevel() {
+        return reorderLevel;
+    }
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+    
+    //Setters
+    public void setMaterialTd(int materialTd) {
+        this.materialTd = materialTd;
+    }
+
+    public void setMaterialName(String materialName) {
+        this.materialName = materialName;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setQuantity(int quantity) {
+        //Prevents negative stock values
+        if (quantity < 0)
+        {
+            this.quantity = 0;
+        }else{      
+            this.quantity = quantity;
+        }
+    }
+
+    public void setReorderLevel(int reorderLevel) {
+        //Prevents negative reOrder values
+        if (reorderLevel < 0)
+        {
+            this.reorderLevel = 0;
+        }else{      
+            this.reorderLevel = reorderLevel;
+        }
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+    
+    //Helper method
+    public boolean isLowStock(){
+       return quantity <= reorderLevel; 
+    }
+    
+}
